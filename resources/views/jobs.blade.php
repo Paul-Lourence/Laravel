383650373b3dd1320b52d0331b39328c3feac32a
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-08-04 16:35:26 +0800

    Laravel Update

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,15 +3,17 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job ['title'] }}:</strong> Pays {{ $job [ 'salary' ]}} per year.    
-                </a>     
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border-gray-200 rounded-lg">
+                <div class="text-blue-500 font-bold text-sm" >{{ $job->employer->name}}</div>
+
+                <div>
+                    <strong>{{ $job ['title'] }}:</strong> Pays {{ $job [ 'salary' ]}} per year.   
+                </div>
+            </a>     
         @endforeach
-    </ul>
+    </div>
 
 
 </x-layout>
