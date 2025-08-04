f3b805df76396c1de67e051db31f1153cbb7a676
Paul Lourence Calicoy <paullourencecalicoy@gmail.com>
2025-08-04 15:12:53 +0800

    Laravel

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,44 +1,23 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
-class Job {
+class Job extends Model {
+    use HasFactory;
 
-    public static function all(): array
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+    
+    public function employer() 
     {
 
-        return [
-            
-            [
-                'id' => '1',
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]          
-        ];
-    } 
-
-    public  static function find(int $id): array
-    {
-        $job =  Arr::first(static::all(), fn($job) => $job['id'] =$id); 
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job; 
+        return $this->belongsTo(Employer::class);
 
     }
 
 };
+
